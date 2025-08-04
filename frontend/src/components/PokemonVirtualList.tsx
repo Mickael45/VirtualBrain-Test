2b03e8b74a6bd2688104d1d597d611ebb69ca05e
Mickael <mickaelgomesconsulting@gmail.com>
2025-08-04 14:54:03 +0200

    feature/virtual-list: Updates itemsPerRow to use the useResponiveColumns hook

--- frontend/src/components/PokemonVirtualList.tsx
+++ frontend/src/components/PokemonVirtualList.tsx
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import { useVirtualizer } from "@tanstack/react-virtual";
 import PokemonCard from "./PokemonCard";
 import type { PokemonView } from "types";
+import { useResponsiveColumns } from "@/hooks/useResponsiveColumns";
 
 interface PokemonVirtualListProps {
   pokemons: PokemonView[];
@@ -9,7 +10,7 @@ interface PokemonVirtualListProps {
 
 const PokemonVirtualList = ({ pokemons }: PokemonVirtualListProps) => {
   const parentRef = useRef(null);
-  const itemsPerRow = 4;
+  const itemsPerRow = useResponsiveColumns();
   const rowCount = Math.ceil(pokemons.length / itemsPerRow);
 
   const rowVirtualizer = useVirtualizer({
@@ -22,11 +23,7 @@ const PokemonVirtualList = ({ pokemons }: PokemonVirtualListProps) => {
   return (
     <div ref={parentRef} className="h-screen w-full overflow-y-auto">
       <div
-        style={{
-          height: `${rowVirtualizer.getTotalSize()}px`,
-          width: "100%",
-          position: "relative",
-        }}
+        className={`relative max-w-7xl mx-auto h-[${rowVirtualizer.getTotalSize()}px]`}
       >
         {rowVirtualizer.getVirtualItems().map((virtualRow) => {
           const startIndex = virtualRow.index * itemsPerRow;
@@ -36,15 +33,12 @@ const PokemonVirtualList = ({ pokemons }: PokemonVirtualListProps) => {
           return (
             <div
               key={virtualRow.key}
+              className="absolute w-full top-0 left-0"
               style={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                width: "100%",
                 transform: `translateY(${virtualRow.start}px)`,
               }}
             >
-              <div className="grid grid-cols-4 justify-items-center p-4">
+              <div className="grid one-card:grid-cols-1 two-cards:grid-cols-2 three-cards:grid-cols-3 four-cards:grid-cols-4 justify-items-center p-4">
                 {rowPokemons.map((pokemon) => (
                   <PokemonCard key={pokemon.id} pokemon={pokemon} />
                 ))}
