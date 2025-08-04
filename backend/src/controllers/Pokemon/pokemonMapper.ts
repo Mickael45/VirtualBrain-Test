356c634ea96deb0113a1a83c8dcd108387e3f8a2
Mickael <mickaelgomesconsulting@gmail.com>
2025-08-04 12:05:48 +0200

    feature/pokemon-endpoints: Cleans up toPokemonView function

--- backend/src/controllers/Pokemon/pokemonMapper.ts
+++ backend/src/controllers/Pokemon/pokemonMapper.ts
@@ -1,21 +1,41 @@
-import { Pokemon, PokemonView } from "types";
+import {
+  Evolution,
+  Pokemon,
+  PokemonView,
+  PreEvolution,
+  StatBlockView,
+} from "types";
+
+const toStatBlockView = ({
+  HP,
+  attack,
+  defense,
+  speed,
+}: Pokemon["stats"]): StatBlockView => ({
+  HP,
+  attack,
+  defense,
+  speed,
+});
+
+const toTypesView = (types: Pokemon["apiTypes"]): string[] =>
+  types.map(({ name }) => name);
+
+const toEvolutionView = (
+  preEvolution: PreEvolution | "none"
+): Evolution | null =>
+  preEvolution !== "none"
+    ? {
+        name: preEvolution.name,
+        pokedexId: preEvolution.pokedexIdd,
+      }
+    : null;
 
 export const toPokemonView = (pokemon: Pokemon): PokemonView => ({
   id: pokemon.id,
   name: pokemon.name,
   image: pokemon.image,
-  stats: {
-    HP: pokemon.stats.HP,
-    attack: pokemon.stats.attack,
-    defense: pokemon.stats.defense,
-    speed: pokemon.stats.speed,
-  },
-  types: pokemon.apiTypes.map(({ name }) => name),
-  evolvesFrom:
-    pokemon.apiPreEvolution !== "none"
-      ? {
-          name: pokemon.apiPreEvolution.name,
-          pokedexId: pokemon.apiPreEvolution.pokedexIdd,
-        }
-      : null,
+  stats: toStatBlockView(pokemon.stats),
+  types: toTypesView(pokemon.apiTypes),
+  evolvesFrom: toEvolutionView(pokemon.apiPreEvolution),
 });
