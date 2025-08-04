00193f0f8e725fd67018d9ee4b452c0181b1cc08
Mickael <mickaelgomesconsulting@gmail.com>
2025-08-04 23:32:51 +0200

    tweaks: Moves caching logic to Home component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,10 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "@/pages/Home/Home";
 import "./App.css";
-
-const ONE_HOUR_IN_MS = 1000 * 60 * 60;
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: ONE_HOUR_IN_MS,
-    },
-  },
-});
 
 function App() {
   return (
     <div className="min-h-screen grid items-center bg-gradient-to-br from-gray-900 via-slate-800 to-zinc-900">
-      <QueryClientProvider client={queryClient}>
-        <Home />
-      </QueryClientProvider>
+      <Home />
     </div>
   );
 }
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -3,14 +3,26 @@ import LoadingSpinner from "@/components/LoadingSpinner";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import HomeContent from "./components/HomeContent";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const ONE_HOUR_IN_MS = 1000 * 60 * 60;
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: ONE_HOUR_IN_MS,
+    },
+  },
+});
 
 const Home = () => {
   return (
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <Suspense fallback={<LoadingSpinner />}>
-        <HomeContent />
-      </Suspense>
-    </ErrorBoundary>
+    <QueryClientProvider client={queryClient}>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Suspense fallback={<LoadingSpinner />}>
+          <HomeContent />
+        </Suspense>
+      </ErrorBoundary>
+    </QueryClientProvider>
   );
 };
 
