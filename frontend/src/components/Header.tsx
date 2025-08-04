7a7405262979799ecac82029ad40b2d6a2f31ef0
Mickael <mickaelgomesconsulting@gmail.com>
2025-08-04 22:42:23 +0200

    feature/pokemon-search: Adds filtering by search term

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,25 @@
+import logo from "@/assets/logo.webp";
+
+interface HeaderProps {
+  searchTerm: string;
+  setSearchTerm: (term: string) => void;
+}
+
+const Header = ({ searchTerm, setSearchTerm }: HeaderProps) => (
+  <header className="bg-gray-700/40 py-4 mb-4">
+    <div className="w-7xl mx-auto">
+      <img src={logo} alt="Logo" className="h-12 mx-auto" />
+      <div className="flex justify-center mt-4">
+        <input
+          type="text"
+          placeholder="Search Pokémon..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-yellow-400 w-full max-w-md"
+        />
+      </div>
+    </div>
+  </header>
+);
+
+export default Header;
